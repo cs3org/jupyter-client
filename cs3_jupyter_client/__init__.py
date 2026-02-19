45abf2e6a31529a11d560207f7b8cbe778ac81ab
Tomas Roun <tomas.roun8@gmail.com>
2026-02-19 12:49:09 +0100

    Add lab extension

--- cs3_jupyter_client/__init__.py
+++ cs3_jupyter_client/__init__.py
@@ -2,3 +2,10 @@ def _jupyter_server_extension_points():
     return [{
         "module": "cs3_jupyter_client.server_extension"
     }]
+
+
+def _jupyter_labextension_paths():
+    return [{
+        "src": "labextension",
+        "dest": "@cs3org/cs3-jupyter-client",
+    }]
